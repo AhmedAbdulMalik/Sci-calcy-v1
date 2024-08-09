b68781d584c3ca1520ecc191339619a1d94c48ac
AhmedAbdulMalik <abdulmalikandanas@gmail.com>
2024-08-10 01:33:14 +0530

    SWITCH ADDED!!!

--- calculator_c.c
+++ calculator_c.c
@@ -15,15 +15,61 @@
 	int* x =&a;
 	int* y =&b;
 
+	char o;
+
 
     printf("Welcome to the Scientific Calculator! To get started->\n");
 	printf("Enter 2 numbers\n");
 	scanf("%d",&a);
 	scanf("%d",&b);
-   printf("wait...");
-		add(x,y);
-		int result = add(x,y);
-         printf("\nthank you");
+   printf("Enter the letter to choose your Operations");
+	
+	printf("a.Addition");
+	printf("b.Subtraction");
+	printf("c.Multiplication");
+	printf("d.Division");
+	scanf("%c",&o);
+
+	switch(o)
+	{
+			case 'A' :
+			case 'a' :
+         					{	add(x,y);
+								int result = add(x,y);
+         						printf("\nthank you");
+								break;
+            				}
+								 					case 'B' :
+													case 'b' :
+         															{	sub(x,y);
+																		int result = sub(x,y);
+         																printf("\nthank you");
+																		break;
+            														}
+													case 'C' :
+													case 'c' :
+         															{	mult(x,y);
+																		int result = mult(x,y);
+         																printf("\nthank you");
+																		break;
+																	}
+           case 'D' :
+		   case 'd' :
+         					{	div(x,y);
+								int result = div(x,y);
+         						printf("\nthank you");
+								break;
+            				} 														
+               
+
+
+
+
+
+	}	
+
+
+
 
 	}
 		int add(int* d,int* e)
