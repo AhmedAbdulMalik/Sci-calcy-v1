c9082a6b82ddd213518c1ffddcc3e0249c0c67ef
AhmedAbdulMalik <152378144+AhmedAbdulMalik@users.noreply.github.com>
2024-08-12 23:13:36 +0530

    Update calculator_c.c

--- calculator_c.c
+++ calculator_c.c
@@ -9,48 +9,48 @@
 		
 		int main(void)
   	{
-	int a=0;
-	 int b=0;
-  	int* x =&a;
-   	int* y =&b;
-
-	char o;
+	 int a=0;
+		 int b=0;
+  			int* x =&a;
+ 			  	int* y =&b;
+					char o;
 
 
  printf("Welcome to the Scientific Calculator! To get started->\n");
-  	printf("Enter 2 numbers\n");
-  	scanf("%d",&a);
-   	scanf("%d",&b);
+  		printf("Enter 2 numbers\n");
+ 		 	scanf("%d",&a);
+			   	scanf("%d",&b);
+		
     printf("Enter the letter to choose your Operations");
 	
 	printf("a.Addition");
- 	printf("b.Subtraction");
-	  printf("c.Multiplication");
-   	printf("d.Division");
-    	scanf("%c",&o);
+ 		printf("b.Subtraction");
+		  printf("c.Multiplication");
+   			printf("d.Division");
+    				scanf("%c",&o);
 
 	switch(o)
 	{
-			case 'A' :
-			case 'a' :
-         					{	add(x,y);
-								int result = add(x,y);
-         						printf("\nthank you");
-								break;
+	case 'A' :
+	case 'a' :
+        	{	add(x,y);
+			 int result = add(x,y);
+         		  printf("\nthank you");
+			   break;
             				}
 
-								 					case 'B' :
-													case 'b' :
-         															{	sub(x,y);
-																		int result = sub(x,y);
-         																printf("\nthank you");
-																		break;
-            														}
-
-													case 'C' :
-													case 'c' :
-         															{	mult(x,y);
-																		int result = mult(x,y);
+						case 'B' :
+						case 'b' :
+         						{	sub(x,y);
+						 		 int result = sub(x,y);
+         			  				  printf("\nthank you");
+					     			   break;
+									         }
+            														
+					                                            case 'C' :
+										    case 'c' :
+         					 					    {	   mult(x,y);
+									                  	int result = mult(x,y);
          																printf("\nthank you");
 																		break;
 																	}
